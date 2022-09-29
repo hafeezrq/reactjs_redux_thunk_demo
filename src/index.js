f965a89c8349e08cbc6d867bcd80b12a0f7dd36c
hafeezrq <hafeezrq@yahoo.com>
2022-09-29 18:06:54 +1000

    Update applicatin setup and make components.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import App from './components/App';
+import reducers from './reducers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={createStore(reducers)}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
