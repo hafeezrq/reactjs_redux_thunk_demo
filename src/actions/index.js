c48c5808db6bf09c8337c57ea2812922ac8e1edd
hafeezrq <hafeezrq@yahoo.com>
2022-10-02 18:39:41 +1100

    Demonstrate use .memoize() of lodash.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
@@ -6,7 +8,10 @@ export const fetchPosts = () => async dispatch => {
 };
 
 export const fetchUser = id => async dispatch => {
-  const response = await jsonPlaceholder.get(`/users/${id}`);
-
-  dispatch({ type: 'FETCH_USER', payload: response.data });
+  _fetchUser(id, dispatch);
 };
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`users/${id}`);
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+});
