26af8a226260e99c77fb701c8468d3eaec8e6ae9
hafeezrq <hafeezrq@yahoo.com>
2022-10-03 18:18:54 +1100

    Refector UserHeader.js

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { fetchUser } from '../actions';
-
 export class UserHeader extends Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.id);
-  }
   render() {
     const { user } = this.props;
     if (!user) {
@@ -20,4 +15,4 @@ const mapStateToProps = (state, ownProps) => {
   return { user: state.users.find(user => user.id === ownProps.id) };
 };
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
